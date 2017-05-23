ea3e2a6447aad05d370fa8ec892f43fc1bd6cf63
Saulo Oliveira <santesoliveira@gmail.com>
2017-05-22 21:10:22 -0300

    Bug fix in metricIsLower function

--- src/roteador/TabelaRoteamento.java
+++ src/roteador/TabelaRoteamento.java
@@ -87,8 +87,6 @@ public class TabelaRoteamento {
         for (Tabela tabelaRot : tabelaRoteamento) {
             if(tabelaRot.getIp_destino().equals(newLine.getIp_destino()) && tabelaRot.getMetrica() > newLine.getMetrica()){
                 return true;
-            }else{
-                return false;
             }
         }
         
